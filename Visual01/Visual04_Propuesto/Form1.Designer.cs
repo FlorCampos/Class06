d62914aac83f5daf10d95fb88c07d2288a651f0b
Flor Campos Flores <u201601859@upc.edu.pe>
2019-12-22 16:55:44 -0500

    more exercises

--- Visual01/Visual04_Propuesto/Form1.Designer.cs
+++ Visual01/Visual04_Propuesto/Form1.Designer.cs
@@ -32,58 +32,240 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Location = new System.Drawing.Point(27, 29);
+            this.groupBox1.Location = new System.Drawing.Point(27, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(225, 172);
+            this.groupBox1.Size = new System.Drawing.Size(324, 155);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Lista de CDs.";
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.AntiqueWhite;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(18, 27);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(175, 108);
+            this.listBox1.Size = new System.Drawing.Size(288, 108);
             this.listBox1.TabIndex = 0;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
             this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Location = new System.Drawing.Point(310, 29);
+            this.groupBox2.Location = new System.Drawing.Point(442, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.Size = new System.Drawing.Size(243, 71);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Adicionar nuevo CD";
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.AntiqueWhite;
             this.textBox1.Location = new System.Drawing.Point(16, 29);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Eliminar CD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Image = global::Visual04_Propuesto.Properties.Resources.osito;
+            this.label2.Location = new System.Drawing.Point(442, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 196);
+            this.label2.TabIndex = 4;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Location = new System.Drawing.Point(27, 321);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(658, 148);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Temas Para:                                     ";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 12;
+            this.label5.UseWaitCursor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(473, 34);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(160, 95);
+            this.listBox3.TabIndex = 1;
+            this.listBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox3_MouseClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(24, 34);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(453, 95);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.textBox2.Location = new System.Drawing.Point(27, 517);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(431, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 564);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Añadir Tema";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(271, 564);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Eliminar tema";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Location = new System.Drawing.Point(485, 517);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 80);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Cuàntos seg.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(189, 486);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(533, 486);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 11;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 374);
+            this.BackgroundImage = global::Visual04_Propuesto.Properties.Resources.back06;
+            this.ClientSize = new System.Drawing.Size(742, 620);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,6 +275,20 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Visual01/Visual04_Propuesto/Form1.cs
+++ Visual01/Visual04_Propuesto/Form1.cs
@@ -16,5 +16,169 @@ namespace Visual04_Propuesto
         {
             InitializeComponent();
         }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                String texto = textBox1.Text.Trim();
+                if (texto.Length > 0)
+                {
+                    listBox1.Items.Add(texto);
+                    textBox1.Text = "";
+                    label1.Text = listBox1.Items.Count.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Digite texto");
+                }
+                textBox1.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                label1.Text = listBox1.Items.Count.ToString();
+                label5.Text = " ";
+            }
+
+           else
+            {
+                MessageBox.Show("Seleccione elemento de Lista");
+            }
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                label5.Text = listBox1.SelectedItem.ToString();
+                
+            }
+            else
+            {
+                MessageBox.Show("Seleccione elemento de Lista");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 9; i <= 999; ++i)
+            {
+                comboBox1.Items.Add("" + i); //no permite agregar números                
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String texto = textBox2.Text.Trim(); //Toma lo que hay en la caja de texto y limpia los espacios en blanco
+            String textoDeCombo = comboBox1.Text;
+            if (texto.Length > 0 && comboBox1.SelectedIndex > 0)
+            {
+                listBox2.Items.Add(texto);//Need 1---item es una colección
+                textBox2.Text = "";
+                label3.Text = listBox2.Items.Count.ToString(); //Need 2--actualizo el contador y lo convierto a string
+
+                listBox3.Items.Add(textoDeCombo);
+
+                int sumaValoresListBox3 = 0;
+
+                foreach(object item in listBox3.Items)
+                {
+                    sumaValoresListBox3 += Convert.ToInt32(item);
+                }
+
+                int minutos, segundos;
+
+                minutos = sumaValoresListBox3 / 60;
+                segundos = sumaValoresListBox3 % 60;
+                //label4.Text = sumaValoresListBox3.ToString();
+                label4.Text = minutos.ToString() +"'" + " : " + segundos.ToString() + "''";
+            }
+            else
+            {
+                MessageBox.Show("Digite el nombre del tema y seleccione cantidad de segundos");
+            }
+
+            comboBox1.SelectedIndex = 0;
+            textBox2.Focus(); //el cursor se coloca sobre el textbox 1 para seguir digitando
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                label3.Text = listBox2.Items.Count.ToString();
+
+                listBox3.Items.RemoveAt(listBox3.SelectedIndex);              
+
+                int sumaValoresListBox3 = 0;
+
+                foreach (object item in listBox3.Items)
+                {
+                    sumaValoresListBox3 += Convert.ToInt32(item);
+                }
+
+                int minutos, segundos;
+
+                minutos = sumaValoresListBox3 / 60;
+                segundos = sumaValoresListBox3 % 60;
+                //label4.Text = sumaValoresListBox3.ToString();
+                label4.Text = minutos.ToString() + "'" + " : " + segundos.ToString() + "''";
+
+            }
+
+            else
+            {
+                MessageBox.Show("Seleccione elemento de Lista");
+            }
+        }
+
+        private void listBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                listBox3.SetSelected(listBox2.SelectedIndex, true);
+                
+            }
+
+            else
+            {
+                MessageBox.Show("Seleccione elemento de Lista");
+            }
+
+          
+        }
+
+        private void listBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox3.SelectedIndex != -1)
+            {
+                listBox2.SetSelected(listBox3.SelectedIndex, true);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione elemento de Lista");
+            }
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+
+            }
+
+            else
+            {
+                MessageBox.Show("Seleccione elemento de Lista de CD's");
+            }
+        }
     }
 }
